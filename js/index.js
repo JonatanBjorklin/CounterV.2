1a517caf653d86d02263eae703382ef75152038d
Jonatan Björklin <145354894+JonatanBjorklin@users.noreply.github.com>
2023-10-03 10:55:08 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let resetEl = document.getElementById("reset-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " / "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function decrease() {
+    if (count === 0){
+        count -= 0
+    }
+    else{
+        count -= 1
+        countEl.textContent = count
+    } 
+}
+
+function restart() {
+   window.location.reload();
+}
